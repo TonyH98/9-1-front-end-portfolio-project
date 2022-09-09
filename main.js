4ccda82949508ddd7f9552614a95226717eff616
TonyH98 <tonyhoang@pursuit.org>
2022-09-09 12:57:27 -0400

    HTML finish  and the first Fetch is add

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-const url = `https://animechan.vercel.app/api/quotes`
 
-fetch(url)
-.then((res) => res.json())
-.then((resJson) => {
-    console.log(resJson)
-    // resJson.forEach((element) => {
-    //     console.log(element.anime)
-    // })
-})
-.catch(error => {
-    console.log(error)
-})
+// let classes2 = document.querySelectorAll(".title")
+// let titles = classes2.value.trim().split(' ').join('+')
+// let url2 =  `https://animechan.vercel.app/api/quotes/anime?title=${titles}`
+
+const button = document.querySelectorAll(".button")
+
+button.forEach((buttons) => {
+    buttons.addEventListener("click", (event) => {
+        event.preventDefault()
+        let classes = document.querySelectorAll(".name")
+        classes.forEach((ele) => {
+            let names = ele.value.trim().split(' ').join('+')
+            let url = `https://animechan.vercel.app/api/quotes/character?name=${names}`
+            fetch(url)
+            .then((res) => res.json())
+            .then((resJson) => {
+                resJson.forEach((element) => {
+                    console.log(element)
+                })
+            })
+            .catch((error) => console.log(error))
+        })
+            
+        });
+        
+        
+    })
